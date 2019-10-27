bfd1af7c2b4227dfe932218ab8d6165a12f11929
Voidmian <249453053@qq.com>
2019-10-27 21:38:00 +0800

    EvalRPN unfinished

---
+++ java/explore/Queue/src/MyStack/EvalRPN.java
@@ -0,0 +1,22 @@
+package MyStack;
+
+import java.util.Stack;
+
+/**
+ * @Author Voidmian
+ * @Date 2019/10/26 21:50
+ */
+public class EvalRPN {
+    static public void test(){
+        EvalRPN evalRPN=new EvalRPN();
+        String [] tokens={"2","1","+","3","*"};
+        System.out.println(evalRPN.evalRPN(tokens));
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack <String> RPN =new Stack<>();
+        for (int i = 0; i <tokens.length ; i++) {
+            if
+        }
+    }
+}
